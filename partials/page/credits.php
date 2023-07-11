be1114e37dd14e083b9a38bafec020e889b175c0
Fernando Soares, O Dev <fernandosoares.dev@gmail.com>
2023-07-10 21:49:57 -0300

    Rev4

--- partials/page/credits.php
+++ partials/page/credits.php
@@ -9,24 +9,24 @@
         <div class="row text-center support">
             <div class="col-12 col-md-4">
                 <p>Gabriela Blanchet</p>
-                <p>Antonio Bizzo</p>
+                <p>Antônio Bizzo</p>
                 <p>Carlos Mendonça</p>
                 <p>Chantal Pillet</p>
                 <p>Clarice Martins Costa</p>
             </div>
             <div class="col-12 col-md-4">
-                <p>Cris Tuna</p>
+                <p>Cristina Tuna</p>
                 <p>Flávia Mouta</p>
                 <p>Geraldo Affonso Ferreira</p>
-                <p>Haim Messel</p>
+                <p>Haim Mesel</p>
                 <p>Hsieh Yuan</p>
             </div>
             <div class="col-12 col-md-4">
-                <p>Líria Valverde</p>
-                <p>Lucy Pamboukdijian</p>
+                <p>Liria Knutti</p>
+                <p>Lucy Pamboukdjian</p>
                 <p>Otávio Villares de Freitas</p>
                 <p>Ricardo Mastroti</p>
-                <p>Rodolfo Fucher</p>
+                <p>Rodolfo Fücher</p>
             </div>
         </div>
     </div>
