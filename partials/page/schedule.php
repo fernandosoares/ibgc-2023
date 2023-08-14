e36094a7ea5bed3b4d738b6f0cb146d1cbd33a03
Fernando Soares, O Dev <fernandosoares.dev@gmail.com>
2023-08-14 12:47:06 -0300

    feat: schedule mock

--- partials/page/schedule.php
+++ partials/page/schedule.php
@@ -8,22 +8,30 @@
                 </div>
             </div>
 
-            <!-- <div class="accordion mb-4" id="accordion-programacao">
+            <div class="accordion mb-4" id="accordion-programacao">
 
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="primeiro-dia">
                         <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-primeiro-dia" aria-expanded="true" aria-controls="collapse-primeiro-dia">
-                            <p>DIA DO CONGRESSO</p>
+                            <p>
+                                Dia 1<br />
+                                <span>17 de outubro</span>
+                            </p>
+
                         </button>
                     </h2>
                     <div id="collapse-primeiro-dia" class="accordion-collapse collapse" aria-labelledby="primeiro-dia" data-bs-parent="#accordion-programacao">
                         <div class="accordion-body">
-
+                            <div class="row header">
+                                <div class="col-12 col-md-3">Apresentador</div>
+                                <div class="col-12 col-md-6">| Tema</div>
+                                <div class="col-12 col-md-3">| Horário</div>
+                            </div>
                         </div>
                     </div>
 
                 </div>
-            </div> -->
+            </div>
 
             <div class="col-12 text-center mt-5">
                 <a href="mailto:paula.scalco@ibgc.org.br" class="cta-corporativa">Inscrição corporativa de grupos. Solicite sua proposta</a>
